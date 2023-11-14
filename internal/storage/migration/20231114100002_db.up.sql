223bb2b4ab6ea1a5c0cc481ee35bf7823dae2b3f
stas9132 <ssgolobokov@gmail.com>
2023-11-14 15:11:23 +0300

    upd migrations

--- internal/storage/migration/20231114100002_db.up.sql
+++ internal/storage/migration/20231114100002_db.up.sql
@@ -4,3 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
                                      email VARCHAR(50),
                                      password VARCHAR(50)
 );
+CREATE TABLE IF NOT EXISTS auth (
+                                    id serial PRIMARY KEY,
+                                    login varchar(255) unique not null ,
+                                    password varchar(255));
