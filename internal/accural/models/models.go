845804c13350b7a403ed96d308ea33cf0f34e754
SerjZimmer <rusakov.sd@aobfg.ru>
2023-11-26 16:34:39 +0300

    refactor accural

--- internal/accural/models/models.go
+++ internal/accural/models/models.go
@@ -37,20 +37,16 @@ func (om OrderManager) GetCalculatedDiscountByOrderID(orderID string) (decimal.D
 	return result, nil
 }
 func (om OrderManager) AcceptOrder(order Order) error {
-	var orderID int
-
-	err := om.db.Conn.QueryRow(context.Background(), "INSERT INTO orders DEFAULT VALUES RETURNING order_id").Scan(&orderID)
+	discounts, err := om.GetAllDiscounts()
+	dc, err := order.CalculateDiscount(discounts)
 	if err != nil {
-		om.db.Logger.Error("unable insert into orders table", slog.String("error", err.Error()), slog.String("AcceptOrder", "discounts"))
 		return err
 	}
 
-	for _, good := range order.Goods {
-		_, err = om.db.Conn.Exec(context.Background(), "INSERT INTO discounts (id) VALUES ($1)", orderID, good.Description)
-		if err != nil {
-			om.db.Logger.Error("unable insert into orders table", slog.String("error", err.Error()), slog.String("AcceptOrder", "discounts"))
-			return err
-		}
+	_, err = om.db.Conn.Exec(context.Background(), "INSERT INTO discounts (id) VALUES ($1)", order.Order, dc)
+	if err != nil {
+		om.db.Logger.Error("unable insert into orders table", slog.String("error", err.Error()), slog.String("AcceptOrder", "discounts"))
+		return err
 	}
 
 	return nil
@@ -67,80 +63,46 @@ func (om OrderManager) AcceptDiscount(discount storage.Discount) error {
 	return nil
 }
 
-func (om OrderManager) CalculateDiscount(discounts []storage.Discount) (decimal.Decimal, error) {
+func (o Order) CalculateDiscount(discounts []storage.Discount) (decimal.Decimal, error) {
 	var result decimal.Decimal
-	orders, err := om.GetAllOrders()
-	if err != nil {
-		return result, err
-	}
-	for _, o := range orders {
-		for _, g := range o.Goods {
-			for _, d := range discounts {
-				if !strings.Contains(g.Description, d.Match) {
-					continue
-				}
-				switch d.RewardType {
-				case "%":
-					result = result.Add(g.Price.Mul(d.Reward).Div(decimal.NewFromInt(100)))
-				case "pt":
-					result = result.Add(d.Reward)
-				}
+
+	for _, g := range o.Goods {
+		for _, d := range discounts {
+			if !strings.Contains(g.Description, d.Match) {
+				continue
+			}
+			switch d.RewardType {
+			case "%":
+				result = result.Add(g.Price.Mul(d.Reward).Div(decimal.NewFromInt(100)))
+			case "pt":
+				result = result.Add(d.Reward)
 			}
 		}
-
 	}
 
 	return result, nil
 }
-func (om OrderManager) GetAllOrders() ([]Order, error) {
-	var orders []Order
 
-	rows, err := om.db.Conn.Query(
-		context.Background(),
-		"SELECT order FROM orders",
-	)
+func (om OrderManager) GetAllDiscounts() ([]storage.Discount, error) {
+	var discounts []storage.Discount
+
+	rows, err := om.db.Conn.Query(context.Background(), "SELECT match, reward, reward_type FROM discounts")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var order Order
-		err := rows.Scan(&order.Order)
-		if err != nil {
+		var discount storage.Discount
+		if err := rows.Scan(&discount.Match, &discount.Reward, &discount.RewardType); err != nil {
 			return nil, err
 		}
-
-		goodsRows, err := om.db.Conn.Query(
-			context.Background(),
-			"SELECT description, price FROM goods WHERE order = $1",
-			order.Order,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		var goods []Good
-		for goodsRows.Next() {
-			var good Good
-			err := goodsRows.Scan(&good.Description, &good.Price)
-			if err != nil {
-				return nil, err
-			}
-			goods = append(goods, good)
-		}
-
-		if err := goodsRows.Err(); err != nil {
-			return nil, err
-		}
-
-		order.Goods = goods
-		orders = append(orders, order)
+		discounts = append(discounts, discount)
 	}
 
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
-	return orders, nil
+	return discounts, nil
 }
