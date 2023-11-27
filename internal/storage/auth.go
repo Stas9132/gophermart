73c5efe86605e6bf7e5c1e8b48158752c66df57f
Stas9132 <87331270+Stas9132@users.noreply.github.com>
2023-11-27 15:50:42 +0300

    remove unmarshalJSON

--- internal/storage/auth.go
+++ internal/storage/auth.go
@@ -4,33 +4,12 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
-	"encoding/json"
-	"errors"
 	"log/slog"
 )
 
 type Auth struct {
 	Login    string `json:"login"`
 	Password string `json:"password"`
-}
-
-func (a *Auth) UnmarshalJSON(data []byte) error {
-	type AuthAlias Auth
-	aliasValue := &struct {
-		*AuthAlias
-	}{
-		AuthAlias: (*AuthAlias)(a),
-	}
-	if err := json.Unmarshal(data, aliasValue); err != nil {
-		return err
-	}
-	if len(a.Login) < 4 {
-		return errors.New("login short")
-	}
-	if len(a.Password) < 4 {
-		return errors.New("password short")
-	}
-	return nil
 }
 
 func (s *DBStorage) RegisterUser(auth Auth) (bool, error) {
